9df2ddd174f4ed568ca5eb984922a57347674d19
qoade <as8art@gmail.com>
2024-03-29 23:59:33 +0300

    added parsing

--- main.cpp
+++ main.cpp
@@ -3,17 +3,76 @@
 #include "r.h" // point and vector
 
 #include <iostream>
+#include <fstream>
 #include <vector>
-using namespace std;
+
+std::vector<r> parse_line(const std::string& str) {
+
+	std::vector<double> nums;
+	std::string num;
+	bool numing = false;
+	for (char ch: str) {
+		if (isdigit(ch) || ch == '.') {
+			numing = true;
+			num.push_back(ch);
+		}
+		else {
+			if (num == ".") {
+				numing = false;
+				num.clear();
+			}
+			if (numing) {
+				nums.push_back(std::stod(num));
+				numing = false;
+				num.clear();
+			}
+
+		}
+	}
+	if (numing) {
+		nums.push_back(std::stod(num));
+	}
+
+	// нету проверки для нечётного количества чисел
+	std::vector<r> ret;
+
+	bool firstEmpty = true;
+	double first;
+	double second;
+	for (double num : nums) {
+		if (firstEmpty) {
+			first = num;
+			firstEmpty = false;
+		}
+		else {
+			firstEmpty = true;
+			second = num;
+			ret.push_back(r(first, second));
+		}
+	}
+
+	return ret;
+}
 
 int main() {
+	std::ifstream ifs("input.txt");
 
-    ConvexPolygon A = {{-2, 0}, {0, 2}, {-2, 3}};
-    ConvexPolygon B = {{0, 0}, {2, 0}, {0, 2}};
-    ConvexPolygon C = {{0, 2}, {0, 4}, {2, 4}};
+	std::string str;
 
-    vector<ConvexPolygon> v = {A, B, C};
 
-    intersects(v);
+	int N;
+	getline(ifs, str);
+	N = str[0] - '0';
+
+   std::vector<ConvexPolygon> polygons;
+
+   while (std::getline(ifs, str)) {
+	   std::vector<r> rs = parse_line(str);
+	   polygons.push_back(ConvexPolygon(rs));
+   }
+
+	intersects(polygons);
+
+   ifs.close();
 	return 0;
 }
